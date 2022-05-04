31c0afe74bbefa8f9e75ececb0615c4b0087a8f6
Mildous <onyu95@naver.com>
2022-05-04 17:27:21 +0900

    Java 0504

---
+++ 220504/src/Chap05/DateTest.java
@@ -0,0 +1,20 @@
+package Chap05;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateTest {
+
+	public static void main(String[] args) {
+		Date today = new Date();
+		System.out.println(today);
+		
+		SimpleDateFormat sf = new SimpleDateFormat("yyyy/MM/dd");
+		String td = sf.format(today);
+		
+		System.out.println(td);
+		
+
+	}
+
+}
