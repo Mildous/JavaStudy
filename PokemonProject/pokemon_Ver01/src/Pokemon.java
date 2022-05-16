aad96af4d9e47d162a84c5b6d18fb85e2b856d3f
Mildous <9089_@naver.com>
2022-05-16 22:29:40 +0900

    Pokemon Project Update 0516

---
+++ PokemonProject/pokemon_Ver01/src/Pokemon.java
@@ -0,0 +1,15 @@
+public class Pokemon {
+    String name;
+    int power;
+
+    Pokemon(){
+        this("피카츄", 500);
+    }
+    Pokemon(String name, int power){
+        this.name = name;
+        this.power = power;
+    }
+    void showGetMon() {
+        System.out.println(name+"(CP:"+power+")");
+    }
+}
