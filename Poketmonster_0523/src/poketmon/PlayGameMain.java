075a15059c9e178f22ed009b224513894aa070e4
Mildous <onyu95@naver.com>
2022-05-24 13:56:21 +0900

    ...T,.T..

--- Poketmonster_0523/src/poketmon/PlayGameMain.java
+++ Poketmonster_0523/src/poketmon/PlayGameMain.java
@@ -6,32 +6,34 @@ public class PlayGameMain {
 
     public static void main(String[] args) {
         GameManager gm = new GameManager();
+        BattleMewtwo mewtwo = new BattleMewtwo();
+        
         int choice = 0;
         boolean loginResult = false;
         String num;
 
         gm.readUserFromFile();
-        while (loginResult == false) {
-            MenuViewer.loginMenu();
-            choice = Integer.parseInt(GameConst.sc.nextLine());
-            switch (choice) {
-                case 1:
-                    gm.newLogIn();
-                    break;
-                case 2:
-                    loginResult = gm.login();
-                    choice = 0;
-                    break;
-                case 3:
-                    System.out.println("종료합니다");
-                    gm.saveUserToFile();
-                    System.exit(0);
+		while (loginResult == false) {
+			MenuViewer.loginMenu();
+			choice = Integer.parseInt(GameConst.sc.nextLine());
+			switch (choice) {
+			case 1:
+				gm.newLogIn();
+				break;
+			case 2:
+				loginResult = gm.login();
+				choice = 0;
+				break;
+			case 3:
+				System.out.println("종료합니다");
+				gm.saveUserToFile();
+				System.exit(0);
             }
         }
         gm.readPoketmonsFromFile();
         while (loginResult == true) {
             switch (choice) {
-                case 0: //메인메뉴
+                case 0: 
                     MenuViewer.showMainMenu();
                     try {
                         num = GameConst.sc.nextLine().trim();
@@ -40,7 +42,7 @@ public class PlayGameMain {
                         } else {
                             System.out.println("잘못입력하셨습니다..");
 
-                        } //break;
+                        } 
                         continue;
 
                     } catch (NumberFormatException e) {
@@ -51,14 +53,14 @@ public class PlayGameMain {
 
                     break;
                 case 1:
-                	MenuViewer.showCatchMenu(); //MenuViewer의 지역메뉴 showCatchMenu()호출
+                	MenuViewer.showCatchMenu();
                     try{
                         if(gm.getPoketmon(Integer.parseInt(GameConst.sc.nextLine()))){
-                            //입력받은값(int)을 gm의 selectType으로 넘겨주고 getPoketmon()의 반환값이 true일 경우
-                            choice = 2; //2번메뉴 실행(가방 전체출력)
+                           
+                            choice = 2; 
                         }else{
-                            System.out.println("             메인메뉴로 돌아갑니다.."); // false일 경우
-                            choice = 0; //0번메뉴 실행(초기메뉴)
+                            System.out.println("             메인메뉴로 돌아갑니다.."); 
+                            choice = 0;
                         }
                     } catch (NumberFormatException e) {
                         System.out.println("숫자만 입력하세요...");
@@ -69,26 +71,25 @@ public class PlayGameMain {
                     }
                     break;
                 case 2:
-                    gm.showBagInPokekmon(); //가방의 내용 전체출력
+                    gm.showBagInPokekmon();
                     choice = 0; //초기메뉴로 이동
                     break;
                 case 3:
-                    MenuViewer.deleteMenu(); //저장된 값을 삭제하는 deleteMenu()메서드 호출
-                    try { //예외처리
-                    	gm.showBagInPokekmon(); //내용 출력
+                    MenuViewer.deleteMenu(); 
+                    try {
+                    	gm.showBagInPokekmon();
                         gm.deletePoketmon(Integer.parseInt(GameConst.sc.nextLine()));
                 		System.out.println("             오박사에게 보냈습니다..     ");
                 		System.out.println();
-                    } catch (NumberFormatException e) { // 숫자가 아닐경우
+                    } catch (NumberFormatException e) {
                         System.out.println("숫자만 입력하세요..");
                     } catch (IndexOutOfBoundsException e) {
-                        System.out.println("빈 가방입니다.." + (GameConst.poketmonBag.size())); // 가방이 비었을 경우
+                        System.out.println("             빈 가방입니다.." + (GameConst.poketmonBag.size()));
                     }
-                    choice = 0; //초기메뉴로 이동
+                    choice = 0; 
                     break;
                 case 4:
-
-                    if(GameConst.poketmonBag.size() < 3) {
+                	if(GameConst.poketmonBag.size() < 3) {
                 		System.out.println("             입장하실 수 없습니다..    　");
                 		System.out.println();
                 		choice = 0;
@@ -105,12 +106,10 @@ public class PlayGameMain {
                 				System.out.println("           ["+(j+1)+"]"+" ["+ p.name +"] [ CP "+p.cp+"]");
                 			}
                 		}
-                		System.out.println("           TOTAL " + gm.getTotalCp()); //CP합계 출력
-                		if(gm.getTotalCp() > 200) { //뮤츠 CP설정, 이겼을 경우
-                			System.out.println("                    <승리>");
-                		}else {
-                			System.out.println("                    <패배>");
-                		}
+                		System.err.println();
+                		System.out.println("           [TOTAL CP] " + mewtwo.getTotalCp()); //CP합계 출력
+                		System.out.println("           [뮤츠의　CP] "+mewtwo.MewtwoCP);
+                		mewtwo.battleIf();
                 	}catch (NumberFormatException e) {
                 		System.out.println("            숫자만 입력하세요..");
                 	}
@@ -119,9 +118,9 @@ public class PlayGameMain {
                 	break;
                 case 5:
                     System.out.println("            게임을 종료합니다..");
-                    gm.savePoketmonsToFile();
-                    gm.saveUserToFile();
-                    return; //프로그램 종료
+    				gm.savePoketmonsToFile();
+    				gm.saveUserToFile();
+    				return; // 프로그램 종료
             }
         }
     }
